11c4e9b6d462c704e9e9ced817e8a8acff1be698
AbhiC7721 <abhic7721@gmail.com>
2021-01-22 22:10:00 +0530

    Customizing dynamic pages using handlebars

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+//To set up handlebars
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Abhinaba Chowdhury'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Abhinaba Chowdhury'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        helpMessage: 'We are providing with some helpful message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
